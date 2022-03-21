5dc0e94546b4571fa74f5add2504809a51dea0da
Vangelis <ekakouras@hotmail.gr>
2022-03-21 12:33:54 +0200

    Initial commit

---
+++ web/flask/application.py
@@ -0,0 +1,145 @@
+import os
+import uuid
+from flask import Flask, session,render_template,request, redirect, send_from_directory
+from sqlalchemy.orm import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from db import db_init, db
+from models import  User, Product
+from helpers import login_required
+from config import DEV_DB, PROD_DB
+
+app = Flask(__name__)
+if os.environ.get('DEBUG') == '1':
+    app.config['SQLALCHEMY_DATABASE_URI'] = DEV_DB
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = PROD_DB
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db_init(app)
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.config['SECRET_KEY'] = "123456789"
+Session(app)
+
+#static file path
+@app.route("/static/<path:path>")
+def static_dir(path):
+    return send_from_directory("static", path)
+
+#signup as merchant
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+    if request.method=="POST":
+        session.clear()
+        password = request.form.get("password")
+        repassword = request.form.get("repassword")
+        if password!=repassword:
+            return render_template("error.html", message="Passwords do not match!")
+
+        #hash password
+        pw_hash = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        fullname = request.form.get("fullname")
+        username = request.form.get("username")
+        #store in database
+        new_user =User(fullname=fullname,username=username,password=pw_hash)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except:
+            return render_template("error.html", message="Username already exists!")
+        return render_template("login.html", msg="Account created!")
+    return render_template("signup.html")
+
+#login as merchant
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method=="POST":
+        session.clear()
+        username = request.form.get("username")
+        password = request.form.get("password")
+        result = User.query.filter_by(username=username).first()
+        print(result)
+        # Ensure username exists and password is correct
+        if result == None or not check_password_hash(result.password, password):
+            return render_template("error.html", message="Invalid username and/or password")
+        # Remember which user has logged in
+        session["username"] = result.username
+        return redirect("/home")
+    return render_template("login.html")
+
+#logout
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
+
+#view all products
+@app.route("/")
+def index():
+    rows = Product.query.all()
+    return render_template("index.html", rows=rows)
+
+#merchant home page to add new products and edit existing products
+@app.route("/home", methods=["GET", "POST"], endpoint='home')
+@login_required
+def home():
+    if request.method == "POST":
+        image = request.files['image']
+        filename = str(uuid.uuid1())+os.path.splitext(image.filename)[1]
+        image.save(os.path.join("static/images", filename))
+        category= request.form.get("category")
+        name = request.form.get("pro_name")
+        description = request.form.get("description")
+        price_range = request.form.get("price_range")
+        comments = request.form.get("comments")
+        new_pro = Product(category=category,name=name,description=description,price_range=price_range,comments=comments, filename=filename, username=session['username'])
+        db.session.add(new_pro)
+        db.session.commit()
+        rows = Product.query.filter_by(username=session['username'])
+        return render_template("home.html", rows=rows, message="Product added")
+
+    rows = Product.query.filter_by(username=session['username'])
+    return render_template("home.html", rows=rows)
+
+#when edit product option is selected this function is loaded
+@app.route("/edit/<int:pro_id>", methods=["GET", "POST"], endpoint='edit')
+@login_required
+def edit(pro_id):
+    #select only the editing product from db
+    result = Product.query.filter_by(pro_id = pro_id).first()
+    if request.method == "POST":
+        #throw error when some merchant tries to edit product of other merchant
+        if result.username != session['username']:
+            return render_template("error.html", message="You are not authorized to edit this product")
+        category= request.form.get("category")
+        name = request.form.get("pro_name")
+        description = request.form.get("description")
+        price_range = request.form.get("price_range")
+        comments = request.form.get("comments")
+        result.category = category
+        result.name = name
+        result.description = description
+        result.comments = comments
+        result.price_range = price_range
+        db.session.commit()
+        rows = Product.query.filter_by(username=session['username'])
+        return render_template("home.html", rows=rows, message="Product edited")
+    return render_template("edit.html", result=result)
+
+#when delete product option is selected this function is loaded
+@app.route("/delete/<int:pro_id>", endpoint='delete')
+def delete(pro_id):
+    item_to_delete = Product.query.filter_by(pro_id=pro_id).first()
+    db.session.delete(item_to_delete)
+    db.session.commit()
+    rows = Product.query.filter_by(username=session['username'])
+    return render_template("home.html", rows=rows, message="Product deleted")
+
+# Run this command to run the application in DEBUG mode
+# docker run -p 5000:5000 -e DEBUG=1 flask_app_dev
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=os.environ.get('DEBUG') == '1')
+
